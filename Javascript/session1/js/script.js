a0e87e403760e4e449342982fc97440c1c27e5e1
NouranFayez <nouran_ahmed269@hotmail.com>
2024-08-12 22:07:51 +0300

    bootstrap session 3 & js session 1

---
+++ Javascript/session1/js/script.js
@@ -0,0 +1,110 @@
+// console.log('Javascript')
+// var variableName = variable Value
+var firstName = "Nouran"
+var num = 30
+var isGraduated = true
+var g = "true"
+// console.log(firstName)
+// console.log(num)
+// console.log(isGraduated)
+// console.log(g)
+
+var lastName = 'Ahmed'
+
+// var fullName = firstName+ " " +lastName
+// template string
+var fullName = `my name = ${firstName} ${lastName} `
+var h 
+
+// console.log(fullName)
+// console.log(h)
+
+// var t = null
+// console.log(t)
+
+// var n1 = 20
+// var n2 = 30 
+// var n3 = 8 
+// var n4 = 2
+// console.log(n1 + n2)
+// console.log(n2 - n1)
+// console.log(n3 / n4)
+
+var k = 20
+// console.log(k++) // 20
+// console.log(k) // 21
+// console.log(++k) // 22
+// console.log(++k) // 23
+
+var x = 30
+// console.log(--x)
+
+
+var z = 40
+
+z = z + 5
+// console.log(z) // 45
+
+// console.log(z+=5) // 50
+// console.log(z-=10) // 40
+
+
+//////////////////////////////////////
+
+var n1 = 10 
+var n2 = 20 
+var n3 = 20
+var n4 = "20"
+// console.log(n1 > n2)
+// console.log(n2 <= n3)
+
+// console.log(n2 == n4) // value
+// console.log(n2 === n4) // value - data type
+
+// console.log(n2 != 20);  // value
+// console.log(n2 !== "20");  // value - data type
+
+
+firstName = 'Nouran';
+// console.log(n2 == 20 && firstName == 'Nouran'  );
+
+// console.log(n2 == 30 || firstName == 'Ahmed');
+
+
+// Ternary operators
+
+// (condition) ? true code : false code
+
+// (n2 == 30 || n3 == 20) ? console.log('correct') : console.log('wrong')
+
+
+
+
+// var r = "";
+var r = "string";
+var r = 20;
+var r = 0;
+var r = " ";
+var r = -0;
+var r = true;
+var r = false;
+var r = null;
+var r ;
+
+
+(!r) ? console.log('correct') : console.log('wrong')
+
+
+var n = 20
+var m = "loremn"
+
+console.log(typeof(n))
+console.log(typeof(m))
+console.log(typeof m)
+
+console.log(typeof n == 'string' || n >= 20)
+
+var p = prompt('enter your name')
+
+console.log(p)
+
