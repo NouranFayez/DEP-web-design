b0c60f0bd516c4287d011a7d5e65fa95f43858c2
NouranFayez <nouran_ahmed269@hotmail.com>
2024-10-08 20:28:27 +0300

    session 9

--- Angular/session8 & session9/src/app/app.module.ts
+++ Angular/session8 & session9/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsCategoryComponent } from './pages/products-category/products-category.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -14,7 +14,12 @@ import { GalleryComponent } from './pages/gallery/gallery.component';
 import { GalleryGuardComponent } from './pages/gallery-guard/gallery-guard.component';
 import { ParentComponent } from './pages/parent/parent.component';
 import { ChildComponent } from './pages/child/child.component'
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +36,14 @@ import { ChildComponent } from './pages/child/child.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+  })
     // HttpClientModule
   ],
   providers: [
